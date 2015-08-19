85028f14b06f873479f751c2803439585204a9bb
haiwenli <haiwen.li@dianping.com>
2015-08-19 15:00:00 +0800

    增加 UtcTime类

---
+++ app/src/main/java/com/mobile/hw/time/UtcTime.java
@@ -0,0 +1,13 @@
+package com.mobile.hw.time;
+
+/**
+ * Created with Android Studio.
+ * User: haiwen.li
+ * Date: 8/19/15
+ * Time: 2:59 PM
+ * <p/>
+ * TODO: Add a header comment!
+ */
+
+public class UtcTime {
+}
