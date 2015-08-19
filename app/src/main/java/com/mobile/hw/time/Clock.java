3c4f07d2dc9f68e9c191f6cb9f180162669fabfc
haiwenli <haiwen.li@dianping.com>
2015-08-19 15:08:56 +0800

    增加：Clock类增加静态方法toLocaleTime

--- app/src/main/java/com/mobile/hw/time/Clock.java
+++ app/src/main/java/com/mobile/hw/time/Clock.java
@@ -11,8 +11,12 @@ package com.mobile.hw.time;
 
 public abstract class Clock {
 
-    private final int UTC_OFFSET = 0;
+    private static final int UTC_OFFSET = 0;
     private int localTime = 0;
 
     public abstract void setLocalTime(int localTime);
+
+    public static int toLocaleTime(int utcZeroTime) {
+        return utcZeroTime + UTC_OFFSET;
+    }
 }
