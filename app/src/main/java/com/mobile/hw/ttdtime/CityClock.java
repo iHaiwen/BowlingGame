c8d894f91f8a6f1a4c15718d76d49268aed05ef9
haiwenli <haiwen.li@dianping.com>
2015-08-21 11:01:05 +0800

    增加：描述城市时间的CityClock类

---
+++ app/src/main/java/com/mobile/hw/ttdtime/CityClock.java
@@ -0,0 +1,13 @@
+package com.mobile.hw.ttdtime;
+
+/**
+ * Created with Android Studio.
+ * User: haiwen.li
+ * Date: 8/21/15
+ * Time: 10:54 AM
+ * <p/>
+ * 描述某一城市的时间
+ */
+
+public class CityClock {
+}
