d1c9ac6c1b82e5cf564f298512fc81f1f0615c9d
haiwenli <haiwen.li@dianping.com>
2015-08-21 10:04:11 +0800

    增加：HotelWorldClocksTest类来测试酒店时钟

---
+++ app/src/test/java/com/mobile/hw/bowlinggame/HotelWorldClocksTest.java
@@ -0,0 +1,19 @@
+package com.mobile.hw.bowlinggame;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created with Android Studio.
+ * User: haiwen.li
+ * Date: 8/21/15
+ * Time: 10:01 AM
+ * <p/>
+ * 用TDD方法来实现时钟功能编程
+ */
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
+public class HotelWorldClocksTest {
+}
