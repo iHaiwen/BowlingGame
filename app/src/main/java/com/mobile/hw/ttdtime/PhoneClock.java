4b827e984d984cf6bf063f1614b3921d4a68c254
haiwenli <haiwen.li@dianping.com>
2015-08-21 11:03:12 +0800

    增加：描述手机时间的PhoneClock类

---
+++ app/src/main/java/com/mobile/hw/ttdtime/PhoneClock.java
@@ -0,0 +1,13 @@
+package com.mobile.hw.ttdtime;
+
+/**
+ * Created with Android Studio.
+ * User: haiwen.li
+ * Date: 8/21/15
+ * Time: 11:02 AM
+ * <p/>
+ * 描述手机的时间
+ */
+
+public class PhoneClock {
+}
