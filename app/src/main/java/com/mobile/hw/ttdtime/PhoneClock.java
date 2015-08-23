7d6075bf8c0691b3bace6955cd6ab29446be3921
haiwenli <haiwen.li@dianping.com>
2015-08-23 20:33:20 +0800

    PhoneClock类中增加成员变量time，并赋值

--- app/src/main/java/com/mobile/hw/ttdtime/PhoneClock.java
+++ app/src/main/java/com/mobile/hw/ttdtime/PhoneClock.java
@@ -12,12 +12,14 @@ package com.mobile.hw.ttdtime;
 public class PhoneClock extends Clock {
 
     private HotelWorldClockSystem hotelWorldClockSystem;
+    private int time;
 
     public PhoneClock(int utcOffset) {
         super.utcOffset = utcOffset;
     }
 
     public void setTime(int time) {
+        this.time = time;
         if (this.hotelWorldClockSystem == null) {
             return;
         }
