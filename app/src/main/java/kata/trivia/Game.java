e0bded3714b4433b8f1956eed772ff766687a86c
haiwenli <haiwen.li@dianping.com>
2015-08-24 15:08:33 +0800

    1、Game类修改内容：inline method Game.createRockQuestion()。 2、Game类修改内容：Change method Game.createRockQuestion() to be private。

--- app/src/main/java/kata/trivia/Game.java
+++ app/src/main/java/kata/trivia/Game.java
@@ -23,14 +23,8 @@ public class Game {
             popQuestions.addLast("Pop Question " + i);
             scienceQuestions.addLast(("Science Question " + i));
             sportsQuestions.addLast(("Sports Question " + i));
-            //TODO: inline method Game.createRockQuestion()
-            rockQuestions.addLast(createRockQuestion(i));
+            rockQuestions.addLast("Rock Question " + i);
         }
-    }
-
-    //TODO: Change method Game.createRockQuestion() to be private.
-    public String createRockQuestion(int index) {
-        return "Rock Question " + index;
     }
 
     //TODO: Remove the unused method Game.isPlayable().
