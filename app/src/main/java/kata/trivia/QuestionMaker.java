ef52dea0c084324cd4b9c35c2790a0f13889531d
haiwenli <haiwen.li@dianping.com>
2015-08-24 17:13:09 +0800

    QuestionMaker类添加4个问题添加的方法

--- app/src/main/java/kata/trivia/QuestionMaker.java
+++ app/src/main/java/kata/trivia/QuestionMaker.java
@@ -16,4 +16,20 @@ public class QuestionMaker {
     private LinkedList scienceQuestions = new LinkedList();
     private LinkedList sportsQuestions = new LinkedList();
     private LinkedList rockQuestions = new LinkedList();
+
+    public void addPopQuestion(String popQuestion) {
+        popQuestions.add(popQuestion);
+    }
+
+    public void addScienceQuestion(String scienceQuestion) {
+        scienceQuestions.add(scienceQuestion);
+    }
+
+    public void addSportsQuestion(String sportsQuestion) {
+        sportsQuestions.add(sportsQuestion);
+    }
+
+    public void addRockQuestion(String rockQuestion) {
+        rockQuestions.add(rockQuestion);
+    }
 }
