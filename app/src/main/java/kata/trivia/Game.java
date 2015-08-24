854928dae8f9e70487b5b3a9ab4ed3ce1cc6dc80
haiwenli <haiwen.li@dianping.com>
2015-08-24 16:04:13 +0800

    Game类实现TODO：Rename variable 'winner' to be 'isGameStillInProgress'.

--- app/src/main/java/kata/trivia/Game.java
+++ app/src/main/java/kata/trivia/Game.java
@@ -113,12 +113,11 @@ public class Game {
                         + purses[currentPlayer]
                         + " Gold Coins.");
 
-                //TODO: Rename variable 'winner' to be 'isGameStillInProgress'.
-                boolean winner = didPlayerWin();
+                boolean isGameStillInProgress = didPlayerWin();
                 currentPlayer++;
                 if (currentPlayer == players.size()) currentPlayer = 0;
 
-                return winner;
+                return isGameStillInProgress;
             } else {
                 //TODO: Duplicate code in method Game.wasCorrectlyAnswered().
                 currentPlayer++;
@@ -136,11 +135,11 @@ public class Game {
                     + purses[currentPlayer]
                     + " Gold Coins.");
 
-            boolean winner = didPlayerWin();
+            boolean isGameStillInProgress = didPlayerWin();
             currentPlayer++;
             if (currentPlayer == players.size()) currentPlayer = 0;
 
-            return winner;
+            return isGameStillInProgress;
         }
     }
 
