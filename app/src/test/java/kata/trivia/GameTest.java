2006b831379a6dc678969c4baae1241c40d93b45
haiwenli <haiwen.li@dianping.com>
2015-08-24 14:12:33 +0800

    GameTest类，配置Robolectric配置

--- app/src/test/java/kata/trivia/GameTest.java
+++ app/src/test/java/kata/trivia/GameTest.java
@@ -1,10 +1,17 @@
 package kata.trivia;
 
 
+import com.mobile.hw.bowlinggame.BuildConfig;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
 public class GameTest {
     /**
      * Rigourous Test :-)
