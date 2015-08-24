7cdc3af412364417e72a34fc0ebe5efa6c1a163e
haiwenli <haiwen.li@dianping.com>
2015-08-24 18:11:21 +0800

    GameTest类增加4个TODO来测试QuestionMaker类

--- app/src/test/java/kata/trivia/GameTest.java
+++ app/src/test/java/kata/trivia/GameTest.java
@@ -76,4 +76,9 @@ public class GameTest {
         //Assert
         Assert.assertFalse(isGameStillInProgress);
     }
+
+    //TODO: add two pop questions and could remove the first one
+    //TODO: add two science questions and could remove the first one
+    //TODO: add two sports questions and could remove the first one
+    //TODO: add two rock questions and could remove the first one
 }
