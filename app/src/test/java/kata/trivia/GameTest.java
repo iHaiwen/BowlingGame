57bc8529d911400cb9f4de62396b13b940aeae3f
haiwenli <haiwen.li@dianping.com>
2015-08-24 14:20:39 +0800

    GameTest类添加3个用户意图测试的todo

--- app/src/test/java/kata/trivia/GameTest.java
+++ app/src/test/java/kata/trivia/GameTest.java
@@ -20,4 +20,9 @@ public class GameTest {
     public void testApp() {
         assertEquals(5, 2 + 3);
     }
+
+    //TODO-user-intent: the game should be over if  a player rolls the dice and answers each question correctly for 6 times
+    //TODO-user-intent: the game should be over if a player rolls the dice for 7 times and answers the question wrongly for 1 times followed by an odd rolling number but then correctly for 6 times
+    //TODO-user-intent: the game should be over if a player rolls the dice for 8 times and answers the question wrongly for 1 times followed by an even rolling number but then correctly for 7 times
+
 }
