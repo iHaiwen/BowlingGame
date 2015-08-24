43b173872aa122955a2c1e08673c7404791b37c2
haiwenli <haiwen.li@dianping.com>
2015-08-24 17:04:48 +0800

    1、创建QuestionMaker类 2、Game类实现TODO：Move question lists to a new  class QuestionMaker。

---
+++ app/src/main/java/kata/trivia/QuestionMaker.java
@@ -0,0 +1,19 @@
+package kata.trivia;
+
+import java.util.LinkedList;
+
+/**
+ * Author: haiwen.li
+ * Time: 8/24/15 5:02 PM
+ * <p/>
+ * 问题产生者
+ */
+
+public class QuestionMaker {
+
+    //TODO-working-on: Move question lists to a new  class QuestionMaker
+    private LinkedList popQuestions = new LinkedList();
+    private LinkedList scienceQuestions = new LinkedList();
+    private LinkedList sportsQuestions = new LinkedList();
+    private LinkedList rockQuestions = new LinkedList();
+}
