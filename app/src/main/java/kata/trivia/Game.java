3330d5945eb6544ca806e7f54e74cf85fd630235
haiwenli <haiwen.li@dianping.com>
2015-08-25 10:46:44 +0800

    Game类删除成员变量places

--- app/src/main/java/kata/trivia/Game.java
+++ app/src/main/java/kata/trivia/Game.java
@@ -12,7 +12,6 @@ public class Game {
 
     private ArrayList<Player> players = new ArrayList();
     //TODO-working-on: Move places into class Player
-    private int[] places = new int[6];
     //TODO: Move purses into class Player
     private int[] purses = new int[6];
     //TODO: Move inPenaltyBox into class Player
