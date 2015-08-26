51efef583ff5c58941489fcc42d1bcff025cfdb1
haiwenli <haiwen.li@dianping.com>
2015-08-26 23:39:08 +0800

    Wrote intention code for the test theTicketDispenserShouldDispenseTheTicketNumber11IfGiveATurnNumber11ToIt().

--- app/src/test/java/kata/td/TicketDispenserTest.java
+++ app/src/test/java/kata/td/TicketDispenserTest.java
@@ -45,6 +45,14 @@ public class TicketDispenserTest {
     //TODO-unit-test-working-on: the ticket dispenser should dispense the ticket number 11 if give a turn number 11 to it
     @Test
     public void theTicketDispenserShouldDispenseTheTicketNumber11IfGiveATurnNumber11ToIt() throws Exception {
+        //Arrange
+        MockTurnNumberSequence mockTurnNumberSequence = new MockTurnNumberSequence();
+        mockTurnNumberSequence.arrangeNextTurnNumber(11);
+        TicketDispenser ticketDispenser = new TicketDispenser(mockTurnNumberSequence);
+
+        //Act
+        TurnTicket ticket = ticketDispenser.getTurnTicket();
+
         //Assert
         Assert.assertEquals(11, ticket.getTurnNumber());
     }
