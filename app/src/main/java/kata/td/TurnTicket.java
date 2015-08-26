763d6cab9b69fe14365e3dfb14cdb754aae7c454
haiwenli <haiwen.li@dianping.com>
2015-08-26 17:19:32 +0800

    Add test project: tbc-ticket-dispenser-java

---
+++ app/src/main/java/kata/td/TurnTicket.java
@@ -0,0 +1,16 @@
+package kata.td;
+
+/**
+ * Created by benwu on 14-6-10.
+ */
+public class TurnTicket {
+    private final int turnNumber;
+
+    public TurnTicket(int turnNumber) {
+        this.turnNumber = turnNumber;
+    }
+
+    public int getTurnNumber() {
+        return turnNumber;
+    }
+}
