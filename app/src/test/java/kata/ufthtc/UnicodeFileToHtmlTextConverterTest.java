519153c8c903ccd115be7a1e2b2568f3b070f9b8
haiwenli <haiwen.li@dianping.com>
2015-08-28 00:25:04 +0800

    Added TODO-new-feature: Make the UnicodeFileToHtmlTextConverter working on for not only a file but also a string.

--- app/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ app/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -1,6 +1,11 @@
 package kata.ufthtc;
 
+import com.mobile.hw.bowlinggame.BuildConfig;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 
@@ -8,9 +13,13 @@ import static org.junit.Assert.assertEquals;
  * Created by benwu on 14-6-10.
  */
 
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, emulateSdk = 21)
 public class UnicodeFileToHtmlTextConverterTest {
     @Test
     public void a_trivial_test() {
         assertEquals(5, 2 + 3);
     }
+
+    //TODO-new-feature: Make the UnicodeFileToHtmlTextConverter working on for not only a file but also a string
 }
