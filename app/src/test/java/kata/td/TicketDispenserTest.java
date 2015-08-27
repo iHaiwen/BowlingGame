3856910ddff1980b4e3143b3dccca4e813d05505
haiwenli <haiwen.li@dianping.com>
2015-08-27 17:55:04 +0800

    1. Finished TODO: Depending on a static method violates the Dependency Inversion Principle and Open-Closed Principle. 2. Finished TODO: the ticket dispenser should dispense the ticket number 11 if give a turn number 11 to it.

--- app/src/test/java/kata/td/TicketDispenserTest.java
+++ app/src/test/java/kata/td/TicketDispenserTest.java
@@ -44,7 +44,6 @@ public class TicketDispenserTest {
         Assert.assertEquals(1, (newTicket.getTurnNumber() - previousTicket.getTurnNumber()));
     }
 
-    //TODO-unit-test-working-on: the ticket dispenser should dispense the ticket number 11 if give a turn number 11 to it
     @Test
     public void theTicketDispenserShouldDispenseTheTicketNumber11IfGiveATurnNumber11ToIt() throws Exception {
         //Arrange
