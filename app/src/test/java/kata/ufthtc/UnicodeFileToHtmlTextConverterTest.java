1be20ebb5cd358061e8ec43ecb460a8dc9a4b105
haiwenli <haiwen.li@dianping.com>
2015-08-28 00:33:19 +0800

    Added 3 user intent test TODOs.

--- app/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
+++ app/src/test/java/kata/ufthtc/UnicodeFileToHtmlTextConverterTest.java
@@ -22,4 +22,7 @@ public class UnicodeFileToHtmlTextConverterTest {
     }
 
     //TODO-new-feature: Make the UnicodeFileToHtmlTextConverter working on for not only a file but also a string
+    //TODO-user-intent-test: should convert ampersand
+    //TODO-user-intent-test: should convert greater than and less than
+    //TODO-user-intent-test: should add a line break for new line
 }
