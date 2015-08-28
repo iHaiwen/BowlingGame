3bfbd5c58c680c0826b7d783ff131e3de9021650
haiwenli <haiwen.li@dianping.com>
2015-08-28 10:35:57 +0800

    Finished TODO: Depending on the third party library violates the Dependency Inversion Principle and Open-Closed Principle.

--- app/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
+++ app/src/main/java/kata/ufthtc/UnicodeFileToHtmlTextConverter.java
@@ -35,7 +35,6 @@ public class UnicodeFileToHtmlTextConverter {
         String line = reader.readLine();
         String html = "";
         while (line != null) {
-            //TODO-working-on: Depending on the third party library violates the Dependency Inversion Principle and Open-Closed Principle
             html += this.stringEscaper.escapeHtml(line);
             html += "<br />";
             line = reader.readLine();
